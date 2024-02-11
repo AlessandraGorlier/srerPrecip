361d2ddfc237aa90ca41793f44b9801f1c591fe9
Bailie Wynbelt <wynbeltb@arizona.edu>
2024-02-11 13:24:14 -0700

    add map to general visualization page and update tp correct bar graph

--- app.R
+++ app.R
@@ -121,44 +121,44 @@ pageWelcomeServer <- function(id, parentSession){
 pageSelectUi <- function(id, selectedGauge) {
   ns <- NS(id)
   tagList(
-  fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      h4("Select rain gauges and set year and month"),
-      p("1. Select rain guage (can hover on icons to view location of gauge)"),
-      p("2. Use sliders to choose time period of interest (this includes year and month)"),
-      p("3. Click download or visualize (message will appear to show progress)"),
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          h4("Select rain gauges and set year and month"),
+          p("1. Select rain guage (can hover on icons to view location of gauge)"),
+          p("2. Use sliders to choose time period of interest (this includes year and month)"),
+          p("3. Click download or visualize (message will appear to show progress)"),
 
-      # select gauges
-      selectInput(ns("selectGauges"),
-        label = "Select Rain Gauges:",
-        choices = unique(precipitation$station)),
+          # select gauges
+          selectInput(ns("selectGauges"),
+                      label = "Select Rain Gauges:",
+                      choices = unique(precipitation$station)),
 
-      # select months
-       sliderInput(ns("selectMonths"),
-                  "Month Selection",
-                  min = 1, max = 12, value = c(3, 9)),
+          # select months
+          sliderInput(ns("selectMonths"),
+                      "Month Selection",
+                      min = 1, max = 12, value = c(3, 9)),
 
-      # slider input for years
-      sliderInput(ns("selectYears"),
-                  "Year Selection",
-                  min = 1922, max = max(precipitation$year), value = value, sep=''),
+          # slider input for years
+          sliderInput(ns("selectYears"),
+                      "Year Selection",
+                      min = 1922, max = max(precipitation$year), value = value, sep=''),
 
 
-      downloadButton(ns("downloadData"),"Download data"),
-      actionButton(ns("visualizeData"), "Visualize data"),
-      hr(),
-      p("Re-download the dataset if you make any changes to location, time period or selected drought index"),
-      p("All statistics and figures on other pages are calculated based on the location and time period specified here. The time period selection forces a minimum length of 30 years to ensure enough observations to calculate meaningful drought indices and climate statistics.")
+          downloadButton(ns("downloadData"),"Download data"),
+          actionButton(ns("visualizeData"), "Visualize data"),
+          hr(),
+          p("Re-download the dataset if you make any changes to location, time period or selected drought index"),
+          p("All statistics and figures on other pages are calculated based on the location and time period specified here. The time period selection forces a minimum length of 30 years to ensure enough observations to calculate meaningful drought indices and climate statistics.")
 
-    ),
-    mainPanel(
-      leafletOutput(ns("srerMap"),
-                    height = "800px",
-                    width = "1000px")
+        ),
+        mainPanel(
+          leafletOutput(ns("srerMap"),
+                        height = "800px",
+                        width = "1000px")
+        )
       )
     )
-  )
   )
 } # End of UI
 
@@ -220,22 +220,53 @@ pageSelectServer <- function(id, selectedGauge, selectedYear) {
 pageVisualizationUi <- function(id, selectedGauge, selectedYear){
   ns <- NS(id)
   tagList(
-    plotOutput(outputId = ns("graph"))
+    fluidPage(
+      sidebarLayout(
+        sidebarPanel(
+          leafletOutput(ns("selectedMap"))
+        ),
+        mainPanel(
+          plotOutput(outputId = ns("graph"))
+        )
+      )
+    )
   )
-} # End of UI
+}
 
 pageVisualizationServer <- function(id, selectedGauge, selectedYear) {
   moduleServer(id, function(input, output, session) {
-    output$graph <- renderPlot({
-      req(selectedGauge())
+    output$selectedMap = leaflet::renderLeaflet({
+      leaflet::leaflet() |>
+        leaflet::addTiles() |>
+        #leaflet::addMarkers(data = selectedGauge(), label = selectedGauge()) |>
+        leaflet::addPolygons(data = bounds,
+                             color = "black",
+                             weight = 2) |>
+        leaflet::addPolylines(
+          data = roads,
+          color = "black",
+          weight = 0.5,
+          opacity = 1
+        ) |>
+        #addPolylines(data = pastures, color="lightgreen", weight = 0.5, opacity = 0.5) %>%
+        leaflet::setView(lng = -110.8529,
+                         lat = 31.8331,
+                         zoom = 11)
+    })
 
+    output$graph <- renderPlot({
       filtered <- precipitation %>%
         filter(station %in% selectedGauge(),
-               year == selectedYear()) %>%
+               year >= selectedYear()[1] & year <= selectedYear()[2]) %>%
         group_by(year) %>%
         summarise(avg_precip = mean(precipitation))
 
-      ggplot(filtered, aes(x=year, y=avg_precip)) + geom_bar(stat="identity")
+      ggplot(filtered, aes(x=year, y=avg_precip)) +
+        geom_bar(stat = "identity", fill = "skyblue", color = 'grey') +
+        labs(x = "Year",
+             y = "Average Precipitation",
+             title = paste("Average Annual Precipitation for", selectedGauge())) +
+        theme_bw()
     })
   })
 } # End of Server
