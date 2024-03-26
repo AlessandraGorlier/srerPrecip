d4f8e813835acf9b444a23f9eeda87caa1774794
Bailie Wynbelt <wynbeltb@arizona.edu>
2024-03-26 12:08:34 -0700

    grab new file path for SRER website

--- data/precipitation/mutate_data.R
+++ data/precipitation/mutate_data.R
@@ -1,39 +1,29 @@
 #Bailie Wynbelt
 #wynbeltb@gmail.com
-#The purpose of this script is to run one a month and output a csv with up-to-
-#date precipitation data from active rain guages on the Santa Rita Experimental
+#The purpose of this script is to run once a month and output a csv with up-to-
+#date precipitation data from active rain gauges on the Santa Rita Experimental
 #Range. This script also will estimate missing values (-9999) based on location
 #and modeled values based on month.
+
 library(readxl)
+library(openxlsx)
 library(dplyr)
 library(tidyverse)
 library(lubridate)
 
-#get current dates
-current_year <- format(Sys.Date(), "%Y")
-current_month <- format(Sys.Date(), "%m")
 
-#check if it is january, if it is go back a year
-if (current_month == 1) {
-  current_year <- current_year - 1
-}
+# GRAB CURRENT SRER DATA--------------------------------------------------
+# get current date
+current_date <- Sys.Date()
 
-#decrease month until it matches
-while (current_month != as.numeric(format(current_date, "%m"))) {
-  current_month <- current_month - 1
-  if (current_month == 0) { #handle a zero value with decreasing in for loop
-    current_month <- 12
-    current_year <- current_year - 1
-  }
-}
+# get previous month
+previous_month <- format(as.Date(format(current_date, "%Y-%m-01")), "%Y-%m")
 
+# grab url
+url <- sprintf("https://santarita.arizona.edu/sites/santarita.arizona.edu/files/%s/precip%s.xlsx", previous_month, previous_month)
 
-#srerPrecip_Year_Month.xlsx (e.g., srerPrecip_2022_02.xlsx)
-url <- "https://cales.arizona.edu/srer/file/srerPrecip_%Y_%m.xlsx"
-
-current_path <- sprintf(url, current_year, current_month)
-
-precip_data <- read_excel("data/precipitation/precipitaion.xlsx")
+# read to xlsx
+precip_data <- read.xlsx(url)
 
 month_mapping <- c(JAN = 1, FEB = 2, MAR = 3, APR = 4, MAY = 5, JUN = 6, JUL = 7, AUG = 8, SEP = 9, OCT = 10, NOV = 11, DEC = 12)
 
@@ -54,19 +44,13 @@ active <- precip_data |>
   mutate(month_id = match(month, names(month_mapping))) |>
   filter(year >= 1923)
 
-#There is 606 occurrences of -9999 (missing data)
-#These need to be estimated
-
 
 #Write to a csv in data precipitation folder
 write.csv(active, "data/precipitation/active_gauges_precip.csv", row.names = FALSE)
 
 
-# estimated rain ----------------------------------------------------------
+# MUTATE ESTIMATED DATA --------------------------------------------------
 precip_data <- read_excel("data/precipitation/srer_estimated_monthly_rainfall.xlsx")
-
-#select stations
-#split year month column
 
 month_mapping <- c('JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC')
 
@@ -98,7 +82,7 @@ for_text_file <- precip_data |>
                         'NW','DESST','164','DESRI','HUERF','LIMST',
                         'NE'))
 
-for_text_file<- for_text_file[order(for_text_file$station),]
+for_text_file <- for_text_file[order(for_text_file$station),]
 
 write.csv(merged_data, "data/precipitation/estimated_precip.csv", row.names = FALSE)
 
